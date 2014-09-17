a643fbbd40113d50d3f399b3db59b0b4ccad40d7
Dibya Chakravorty <dibyachakravorty@gmail.com>
2014-09-17 22:30:26 +0530

    added UserProfile app. added rudimentary UserProfile model.

---
+++ issuewise/userprofile/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.conf import settings
+from django.utils.translation import ugettext_lazy as _
+
+
+class UserProfile(models.Model):
+	""" Stores additional information for users """
+	
+	user = models.OneToOneField(settings.AUTH_USER_MODEL, related_name='profile',
+                                verbose_name=_("user"))
