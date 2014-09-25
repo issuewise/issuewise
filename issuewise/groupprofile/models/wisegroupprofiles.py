3aa96dc766e49de2335bfa1ae7b0e6af6e3f90ac
Dibya Chakravorty <dibyachakravorty@gmail.com>
2014-09-25 16:28:09 +0530

    added organization types to group profile

--- issuewise/groupprofile/models/wisegroupprofiles.py
+++ issuewise/groupprofile/models/wisegroupprofiles.py
@@ -1,7 +1,53 @@
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+
 from groupprofile.models.base import BaseGroupProfile
 
 
 class WiseGroupProfile(BaseGroupProfile):
+
+    GROUP_TYPE_LIST = (
+        ('MUS', 'Museums'), 
+        ('ACA', 'Academic institutions'), 
+        ('ADV', 'Advertising agencies'),
+        ('ANT', 'Antarctic agencies'), 
+        ('BEA', 'Beauty organisations'), 
+        ('CLU', 'Clubs and societies'), 
+        ('COM', 'Committees'),
+        ('CMP', 'Companies'), 
+        ('CRE', 'Credit rating agencies'),
+        ('CRI', 'Criminal organizations'), 
+        ('DIA', 'Diaspora organizations'),
+        ('EMP', 'Employment agencies'),
+        ('EUG', 'Eugenics organizations'),  
+        ('FAS', 'Fashion organizations'), 
+        ('GLO', 'Globalization-related organizations'), 
+        ('GOV', 'Government agencies'), 
+        ('ILL', 'Illegal organizations'), 
+        ('INT', 'International organizations'), 
+        ('INV', 'Investment agencies'),  
+        ('MIL', 'Military forces'), 
+        ('MUS', 'Musical groups'),
+        ('NAT', 'National security institutions'), 
+        ('NEW', 'News agencies'), 
+        ('NPO', 'Non-profit organizations'), 
+        ('PAR', 'Paramilitary organizations'), 
+        ('PAY', 'Payment systems organizations'), 
+        ('POL', 'Political organizations'), 
+        ('PRO', 'Professional associations'), 
+        ('RES', 'Research institutes'), 
+        ('RST', 'Restaurants by type'),
+        ('SUP', 'Supraorganizations'),
+        ('TAL', 'Talent and literary agencies'), 
+        ('TAX', 'Taxpayer groups'),
+        ('TOU', 'Tourism agencies'),
+        ('TRA', 'Training organizations'), 
+        ('TRA', 'Travel agencies'),
+        ('OTH', 'Others'),
+) 
+
+    group_type = models.CharField(_('type of group'),
+            max_length = 3, choices = GROUP_TYPE_LIST)
     
     class Meta:
         app_label = 'groupprofile'
