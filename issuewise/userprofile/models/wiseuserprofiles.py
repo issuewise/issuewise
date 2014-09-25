1f8707d7dfb9319572f08e11f1661c9468676916
Dibya Chakravorty <dibyachakravorty@gmail.com>
2014-09-25 07:08:43 +0530

    added few important fields to userprofile

--- issuewise/userprofile/models/wiseuserprofiles.py
+++ issuewise/userprofile/models/wiseuserprofiles.py
@@ -1,6 +1,18 @@
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
 from userprofile.models.base import BaseUserProfile
 
+GENDER_CHOICES = (
+    ('M', 'male'),
+    ('F', 'female'),
+)
+
 class WiseUserProfile(BaseUserProfile):
+
+    date_of_birth = models.DateTimeField(_('born on'))
+    gender = models.CharField(_('gender'), max_length = 50,
+        choices = GENDER_CHOICES)
+    description = models.TextField(_('bio'))
     
     class Meta:
         abstract = True
