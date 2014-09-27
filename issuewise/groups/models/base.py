a4f6c2293996a2e107b8bfdd3a02f591bf22aa9d
Dibya Chakravorty <dibyachakravorty@gmail.com>
2014-09-27 15:01:16 +0530

    basemembership deleted

--- issuewise/groups/models/base.py
+++ issuewise/groups/models/base.py
@@ -2,18 +2,16 @@ from django.db import models
 from django.conf import settings
 from django.utils.translation import ugettext_lazy as _
 
-from groups.utils import creatable_factory, subscribable_factory 
+from groups.utils import user_as_creator_factory
 
 # Get appropriate mixin classes from their respective 
 # factory methods.
 
-CreatableClass = creatable_factory(accounts_version_label = 'latest',
-                                   core_version_label = 'latest')
-SubscribableClass = subscribable_factory(accounts_version_label = 'latest',
-                                           core_version_label = 'latest')
+UserAsCreatorClass = user_as_creator_factory(accounts_version_label = 'latest',
+                                             core_version_label = 'latest')
 
 
-class BaseGroup(CreatableClass):
+class BaseGroup(UserAsCreatorClass):
     """
     ANY CUSTOM GROUP CLASS SHOULD INHERIT THIS MODEL
 
@@ -31,38 +29,8 @@ class BaseGroup(CreatableClass):
     """ 
     name = models.CharField(max_length=200, help_text=_('Required. ' 
         '200 characters or less.'), verbose_name=_('group name'))
-    members = models.ManyToManyField(settings.AUTH_USER_MODEL, 
-        through=settings.GROUP_MEMBERSHIP_MODEL, 
-        verbose_name=_('group members'))
 
     class Meta:
         app_label = 'groups'
         abstract=True
-
-
-class BaseMembership(SubscribableClass):
-    """
-    GROUP MEMBERSHIP CLASS
-    
-    Fields
-
-    required : group
-        Denotes the group. Subscription is deleted when the group 
-        deleted
-
-    required : subscriber
-        Denotes the member (user). Subscription is deleted when
-        the user gets deleted
-
-    auto : subscribed_at
-        Denotes the time at which the user became a member of the
-        group
-    """
-    group = models.ForeignKey(settings.SITE_GROUP_MODEL,
-        verbose_name=_('group'), related_name = '%(app_label)s_%(class)s_set')
-
-
-    class Meta:
-        app_label = 'groups'
-        abstract = True
 
