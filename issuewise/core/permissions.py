12315968e729c07f6d20228c021c5f1805bc9818
Dibya Chakravorty <dibyachakravorty@gmail.com>
2015-06-18 01:59:56 +0200

    rewrote permission classes to be more modular

--- issuewise/core/permissions.py
+++ issuewise/core/permissions.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.shortcuts import get_object_or_404
+from django.core.exceptions import ObjectDoesNotExist
 
 from rest_framework import permissions
 
@@ -12,43 +13,78 @@ def permission_factory(view):
         return ProfilePermissions
     if view in settings.FRIENDSHIP_PERMISSION_VIEW_CLASSES:
         return FriendshipPermissions
-
-
-class ProfilePermissions(object):
+        
+        
+class Relationship(object):
 
     @classmethod
-    def is_permitted(cls, request, model, obj, url_capture):
+    def request_type(cls, request, obj, owner):
+        # permission classes for this family
+        
+        friends = WiseFriendship.objects.check_friendship(request.user,owner)
+        if friends:
+            request_type ='friend'
+        elif owner == request.user:
+            request_type = 'owner'
+        else:
+            request_type = 'stranger'
+        print request_type    
+        return request_type
+
+    
+
+
+class ProfilePermissions(Relationship):
+
+
+    @classmethod
+    def is_permitted(cls, request, obj, owner):
+        
+        request_type= cls.request_type(request, obj, owner)
+        
         if obj:
-            owner = obj.owner()
             if request.method in permissions.SAFE_METHODS:
                 if obj.privacy == 'A':  
                     return True
-                friends = WiseFriendship.objects.check_friendship(request.user,owner)
-                if friends:
+                elif request_type == 'friend':
                     return True
-            return owner == request.user
-        user_model = get_model_from_settings(settings.AUTH_USER_MODEL)   
-        owner = get_object_or_404(user_model , uri_name = url_capture['uri_name'])
+                elif request_type == 'owner':
+                    return True
+                return False
         return owner == request.user
         
  
-class FriendshipPermissions(object):
+class FriendshipPermissions(Relationship):
  
     @classmethod
-    def is_permitted(cls, request, model, obj, url_capture):
-        user_model = get_model_from_settings(settings.AUTH_USER_MODEL)   
-        owner = get_object_or_404(user_model , uri_name = url_capture['uri_name'])
+    def is_permitted(cls, request, obj, owner ):
+    
+        request_type= cls.request_type(request, obj, owner)
+            
+        # method wise permission assignment
+        
         if request.method in permissions.SAFE_METHODS:
-            friends = WiseFriendship.objects.check_friendship(request.user,owner)
-            if friends:
+            if request_type == 'friend':
                 return True
-            return owner == request.user
+            elif request_type == 'stranger':
+                if WiseFriendship.objects.is_mutual_friend(owner, request.user, obj.follower):
+                    return True
+                return False 
+            elif request_type =='owner':
+                return True 
+            return False
         if request.method == 'PUT':
-            return owner == request.user
+            if request_type == 'owner':
+                return True
+            return False
         if request.method == 'DELETE':
-            return owner == request.user
+            if request_type == 'owner':
+                return True
+            return False
         if request.method == 'POST':
-            return owner != request.user
+            if request_type == 'owner':
+                return False
+            return True
            
             
          
