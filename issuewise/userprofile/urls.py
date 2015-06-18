38bb96836a822319bb727fb2c194926bdd6b43cc
Dibya Chakravorty <dibyachakravorty@gmail.com>
2015-06-18 02:04:15 +0200

    added the friends node and the root node under profile

--- issuewise/userprofile/urls.py
+++ issuewise/userprofile/urls.py
@@ -2,10 +2,16 @@ from django.conf.urls import url
 
 from rest_framework.urlpatterns import format_suffix_patterns
 
-from userprofile.views import PersonalInfo, SocialLinkList, SocialLinkDetail
+from userprofile.views import (PersonalInfo, SocialLinkList, SocialLinkDetail,
+                            FriendshipList, Profile)
 
 
 urlpatterns =[
+    url(
+        regex = r'^$',
+        view = Profile.as_view(),
+        name = 'profile',
+        ),
     url(
         regex = r'^personal-info/$',
         view = PersonalInfo.as_view(),
@@ -20,8 +26,12 @@ urlpatterns =[
         regex = r'^social-links/(?P<pk>.+)$',
         view = SocialLinkDetail.as_view(),
         name = 'social-link-detail',
-        )
-    
+        ),
+    url(
+        regex = r'^friends/$',
+        view = FriendshipList.as_view(),
+        name = 'friendshiplist',
+        ),
 ]
 
 
