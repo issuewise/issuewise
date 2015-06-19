05255ab03d6c9d9cb1caa61276aed62b0f317bb8
Dibya Chakravorty <dibyachakravorty@gmail.com>
2015-06-19 15:35:01 +0200

    moving url generating functions to serilaizer classes

--- issuewise/core/serializers/social_link_serializers.py
+++ issuewise/core/serializers/social_link_serializers.py
@@ -1,13 +1,14 @@
 from django.conf import settings
 from django.utils.translation import ugettext_lazy as _ 
 
-from issuewise.utils import get_model_from_settings
-
 from rest_framework import serializers
+from rest_framework.reverse import reverse
+
+from issuewise.utils import get_model_from_settings
 
 class SocialLinkSerializer(serializers.ModelSerializer):  
 
-    url = serializers.URLField(source = 'get_absolute_url', read_only=True,
+    href = serializers.SerializerMethodField('get_social_link_url', read_only=True,
         help_text = _('url for this social link'))
     website = serializers.CharField(source = 'get_link_type_display',
         read_only = True, help_text=_('website corresponding to this link \
@@ -16,6 +17,13 @@ class SocialLinkSerializer(serializers.ModelSerializer):
     privacy_description = serializers.CharField(source = 'get_privacy_display',
         read_only = True, help_text = _('a help text for understanding the current \
         privacy setting'))
+        
+    def get_social_link_url(self,obj):
+        uri_name = obj.autobiographer.uri_name
+        pk = obj.id
+        return reverse('accounts:userprofile:social-link-detail', 
+            kwargs = {'uri_name' : uri_name, 'pk' : pk}, request = self.context['request'])
+        
 
     class Meta:
         social_link_model = get_model_from_settings(settings.SOCIAL_LINK_MODEL)
