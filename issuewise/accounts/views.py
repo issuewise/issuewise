7ddbba704a4c2c511500f75ec174ea5aeebed151
Dibya Chakravorty <dibyachakravorty@gmail.com>
2015-06-19 15:42:48 +0200

    attribute username in token view class changed to email, activation link ndoes and password reset nodes are now hyperlinked

--- issuewise/accounts/views.py
+++ issuewise/accounts/views.py
@@ -2,19 +2,19 @@ from django.shortcuts import get_object_or_404
 from django.conf import settings
 
 from rest_framework import generics
-from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework.authtoken.models import Token
 from rest_framework.authentication import BasicAuthentication, TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.exceptions import PermissionDenied
 from rest_framework import renderers
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authtoken.models import Token
+from rest_framework.reverse import reverse
 
 from core.views import PermissionMixin, WiseListCreateAPIView
 from accounts.models import WiseUser, WiseActivation, WiseFriendship
-from accounts.serializers import WiseUserSerializer, WiseFriendshipSerializer
-from accounts.exceptions import UserNotActive
+from accounts.serializers import WiseUserSerializer, AuthTokenSerializer
 from issuewise.utils import get_model_from_settings
 
 
@@ -29,6 +29,8 @@ class Accounts(generics.CreateAPIView):
         
         Permission : Anonymous. Everyone is allowed access. 
         
+        
+        
         ---
         
         responseMessages:
@@ -38,148 +40,14 @@ class Accounts(generics.CreateAPIView):
               Check the json response to find out which one of them happened.
         
         """
-        return super(Accounts, self).post(request, *args, **kwargs)
-    
-    
-class ActivationLinkCheck(APIView):
-
-    def get(self, request, uuid, format = None):
-        """
-        
-        Function : Activates the user associated with this resource. 
-        
-        Permission : Anonymous. Everyone is allowed access
-        
-        ---
-        
-        omit_parameters:
-            - path
-        
-        """ 
-        obj = get_object_or_404(WiseActivation, uuid = uuid)
-        user = obj.creator
-        user.activate()
-        token, created = Token.objects.get_or_create(user=user)
-        uri_name = user.uri_name
-        return Response({'token': token.key, 'uri_name' : uri_name})
-        
-        
-class PasswordResetLinkCreate(APIView):
-
-    usermodel = get_model_from_settings(settings.AUTH_USER_MODEL)
-
-    def post(self, request, *args, **kwargs):
-        email = request.data['email']
-        user = self.usermodel.objects.get(email = email)
-        user.send_password_reset_email()    
-        return Response({'message' : 'the password reset link was sent to your email'})
-        
-        
-class PasswordResetLinkCheck(APIView):
-
-    def get(self, request, uuid, *args, **kwargs):
-    
-        password_reset_link_model = settings.PASSWORD_RESET_LINK_MODEL
-
-        obj = get_object_or_404(password_reset_link_model, uuid = uuid)
-        user = obj.creator
-        uri_name = user.uri_name
-        token, created = Token.objects.get_or_create(user=user)
-        obj.delete()
-        return Response({
-                        'token': token.key, 
-                        'link' : reverse('accounts:password',
-                            kwargs = {'uri_name' : uri_name}), 
-                        'method' : 'post',
-                        }
-                        )
-    
-        
-    
-class Password(APIView):
-
-    authentication_classes = (TokenAuthentication,)
-    usermodel = get_model_from_settings(settings.AUTH_USER_MODEL)
-
-    def post(self, request, *args, **kwargs):
-        requesting_user = request.user
-        owner = self.usermodel.objects.get(uri_name = self.kwargs['uri_name'])
-        if requesting_user == owner:
-            password = request.data['password']
-            owner.set_password(password)
-            owner.save()
-            return Response({'message' : 'password was successfully changed'})
-        else:
-            raise PermissionDenied(detail="you are trying to change someone else's password")
-            
-            
-        
-        
-    
-
-        
-    
-    
-
-    
-
-    
-        
-        
-class ActivationLinkCreate(APIView):
-    
-    authentication_classes = (BasicAuthentication,)
-    permission_classes = (IsAuthenticated,)
-
-    def post(self, request, format = None):
-        """
-        Function : Performs the following activities 
-        
-            1. Creates an activation link
-            
-            2. Associates the authenticated user with the activation link 
-            
-            3. Sends an email to the email id of the authenticated user. This
-            email contains the activation link.  
-        
-        Permission : Authenticated user only.IMPORTANT NOTE : This is the only 
-        node where authentication is performed via Basic Http Authentication and 
-        NOT Token Authentication.
-        
-        ---
-        
-        omit_parameters:
-            - path
-            
-        type:
-            token:
-                required: true
-                type: string
-                description: token used for http Token Authentication
-            uri_name:
-                required: true
-                type: string
-                description: uri friendly name of the user. all nodes with \
-                {uri_node} needs to include this string in place of {uri_name}.
-            
-        responseMessages:
-            - code: 403
-              message: This error occurs if an user tries to activate \
-              someone else's account. 
-              
-            - code: 401
-              message : This error occurs if the request cannot be authenticated. \
-              This usually means that the username and password supplied in \
-              the header is wrong or the client is using a protocol different \
-              from Http Basic Authentication to access this node. 
-        
-        """ 
-        user = request.user
-        user.send_activation_email()
-        return Response()
-        
+        response = super(Accounts, self).post(request, *args, **kwargs)
+        response.data['activation_link'] = reverse('accounts:activationlinkcreate',
+                                            request = request)
+        return response
         
 class ObtainTokenForActivatedUsers(ObtainAuthToken):
+
+    serializer_class = AuthTokenSerializer
 
     def post(self, request):
         """
@@ -227,10 +95,169 @@ class ObtainTokenForActivatedUsers(ObtainAuthToken):
         serializer.is_valid(raise_exception=True)
         user = serializer.validated_data['user']
         if user.activity_status == 'I':
-            raise UserNotActive(explanation = user.explanation)
+            if user.explanation == 'NV':
+                return Response({
+                            'detail' : user.get_explanation_display(),
+                            'message': 'looks like your email has not been verified.',
+                            'activation_link' : reverse('accounts:activation-link-create'),
+                            'email' : user.email
+                            }, 
+                            status = status.HTTP_403_FORBIDDEN)
+            else:
+                return Response({
+                            'detail' : user.get_explanation_display(),
+                            },
+                            status = status.HTTP_403_FORBIDDEN)
         token, created = Token.objects.get_or_create(user=user)
         uri_name = user.uri_name
         return Response({'token': token.key, 'uri_name' : uri_name})
+    
+    
+class ActivationLinkCheck(APIView):
+
+    def get(self, request, uuid, format = None):
+        """
+        
+        Function : Activates the user associated with this resource. 
+        
+        Permission : Anonymous. Everyone is allowed access
+        
+        ---
+        
+        omit_parameters:
+            - path
+        
+        """ 
+        obj = get_object_or_404(WiseActivation, uuid = uuid)
+        user = obj.creator
+        user.activate()
+        token, created = Token.objects.get_or_create(user=user)
+        uri_name = user.uri_name
+        return Response({'token': token.key, 
+                        'profile_url' : reverse('accounts:userprofile:profile', 
+                            request = request),
+                        }
+                        )
+                        
+        
+        
+class PasswordResetLinkCreate(APIView):
+
+    usermodel = get_model_from_settings(settings.AUTH_USER_MODEL)
+
+    def post(self, request, *args, **kwargs):
+        email = request.data['email']
+        user = self.usermodel.objects.get(email = email)
+        user.send_password_reset_email()    
+        return Response({
+                        "message" : "An email with the password reset link has been successfully \
+sent to you. If you didn't receive it, try another time.",
+                        "email" : user.email,
+                        'activation_link' : reverse('accounts:password-reset-link-create'),
+                        }
+                        )
+        
+        
+class PasswordResetLinkCheck(APIView):
+
+    def get(self, request, uuid, *args, **kwargs):
+    
+        password_reset_link_model = settings.PASSWORD_RESET_LINK_MODEL
+
+        obj = get_object_or_404(password_reset_link_model, uuid = uuid)
+        user = obj.creator
+        uri_name = user.uri_name
+        token, created = Token.objects.get_or_create(user=user)
+        obj.delete()
+        return Response({
+                        'token': token.key, 
+                        'link' : reverse('accounts:password',
+                            kwargs = {'uri_name' : uri_name}), 
+                        'method' : 'post',
+                        }
+                        )
+    
+        
+    
+class Password(APIView):
+
+    authentication_classes = (TokenAuthentication,)
+    usermodel = get_model_from_settings(settings.AUTH_USER_MODEL)
+
+    def post(self, request, *args, **kwargs):
+        requesting_user = request.user
+        owner = self.usermodel.objects.get(uri_name = self.kwargs['uri_name'])
+        if requesting_user == owner:
+            password = request.data['password']
+            owner.set_password(password)
+            owner.save()
+            return Response({
+                            'message' : 'password was successfully changed',
+                            'profile_url' : reverse('accounts:userprofile:profile', 
+                            request = request),
+                            }
+                            )
+        else:
+            raise PermissionDenied(detail="you are trying to change someone else's password")
+            
+            
+class ActivationLinkCreate(APIView):
+    
+    usermodel = get_model_from_settings(settings.AUTH_USER_MODEL)    
+
+    def post(self, request, format = None):
+        """
+        Function : Performs the following activities 
+        
+            1. Creates an activation link
+            
+            2. Associates the authenticated user with the activation link 
+            
+            3. Sends an email to the email id of the authenticated user. This
+            email contains the activation link.  
+        
+        Permission : Authenticated user only.IMPORTANT NOTE : This is the only 
+        node where authentication is performed via Basic Http Authentication and 
+        NOT Token Authentication.
+        
+        ---
+        
+        omit_parameters:
+            - path
+            
+        type:
+            token:
+                required: true
+                type: string
+                description: token used for http Token Authentication
+            uri_name:
+                required: true
+                type: string
+                description: uri friendly name of the user. all nodes with \
+                {uri_node} needs to include this string in place of {uri_name}.
+            
+        responseMessages:
+            - code: 403
+              message: This error occurs if an user tries to activate \
+              someone else's account. 
+              
+            - code: 401
+              message : This error occurs if the request cannot be authenticated. \
+              This usually means that the username and password supplied in \
+              the header is wrong or the client is using a protocol different \
+              from Http Basic Authentication to access this node. 
+        
+        """ 
+        email = request.data['email']
+        user = self.usermodel.objects.get(email = email)
+        user.send_activation_email()
+        return Response({
+                        "message" : "An email with the activation link has been successfully \
+sent to you. If you didn't receive it, try another time.",
+                        "email" : user.email,
+                        'activation_link' : reverse('accounts:activation-link-create'),
+                        }
+                        )
         
         
         
@@ -240,10 +267,12 @@ class AcceptRejectFriend(PermissionMixin, APIView):
     
     permission_classes = (IsAuthenticated,)
     
+    friendshipmodel = get_model_from_settings(settings.FRIENDSHIP_MODEL)
+    
     
     
     def get_object(self):
-        obj = get_object_or_404(WiseFriendship, id = self.kwargs['pk'])
+        obj = get_object_or_404(self.friendshipmodel, id = self.kwargs['pk'])
         return obj
         
     @property    
@@ -251,7 +280,7 @@ class AcceptRejectFriend(PermissionMixin, APIView):
         try:
             return self._owner
         except AttributeError:
-            obj = get_object_or_404(WiseFriendship, id = self.kwargs['pk'])
+            obj = get_object_or_404(self.friendshipmodel, id = self.kwargs['pk'])
             self._owner = obj.followee
             return self._owner
      
@@ -297,8 +326,10 @@ someone else' , 'stranger' : 'you cannot accept a friend request for someone els
         if obj.status == 'R':
             obj.status = 'F'
             obj.save()
-            WiseFriendship.objects.create(follower = obj.followee, followee = obj.follower, status ='F')
-        return Response()
+            self.friendshipmodel.objects.create(follower = obj.followee, followee = obj.follower, status ='F')
+            return Response({'message' : 'you are now friends with this person'})
+        else:
+            return Response({'message' : 'you are already friends with this person'})
         
     def delete(self, request, format = None, *args, **kwargs):
     
@@ -334,10 +365,12 @@ someone else' , 'stranger' : 'you cannot delete a friend request for someone els
         self.check_permission(obj)
         if obj.status == 'F':
             obj.delete()
-            WiseFriendship.objects.get(follower = obj.followee, followee = obj.follower).delete() 
+            self.friendshipmodel.objects.get(follower = obj.followee, followee = obj.follower).delete() 
+            return Response({'message': 'you are no longer friends with this person'})
         else:
             obj.delete()
-        return Response()
+            return Response({'message': 'the friend request was permanently deleted'})
+        
         
         
             
